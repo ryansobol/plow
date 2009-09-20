08ff6b1e9ad6e90fba68f45d2f424bc3d801bb27
Ryan Sobol <code@ryansobol.com>
2009-09-20 09:07:17 +0800

    * Plow::Application.run! additions:   * parses arguments   * generates a usage message * plow_spec.rb now expects correct version number for Plow::VERSION and VERSION rubygems file * Added spec/spec.opts with default command-line options * Expanding the lib/ path when added onto the $LOAD_PATH from spec_helper.rb

--- lib/plow/application.rb
+++ lib/plow/application.rb
@@ -6,7 +6,30 @@ class Plow
   class Application
     class << self
       def run!(*arguments)
-        ## parse arguments
+        if arguments.length < 2
+          abort <<-MESSAGE
+Usage: plow USER_NAME SITE_NAME [SITE_ALIAS ...]
+
+  Arguments:
+    USER_NAME       Name of a Linux system account user
+    SITE_NAME       Name of the website (e.g. www.apple.com)
+    SITE_ALIAS      (Optional) List of alias names of the website (e.g. apple.com)
+
+  Summary:
+    Plows the fertile soil of your filesystem into neatly organized plots of website templates
+
+  Description:
+    1. Ensure both a system user account and user home exist
+    2. Lay the foundation of a simple website home
+    3. Generate and install a VirtualHost apache2 configuration
+
+  Example:
+    plow apple-steve www.apple.com apple.com
+MESSAGE
+        end
+
+        user_name, site_name = arguments.first(2)
+        site_aliases = arguments.drop(2)
         
         begin
           generator = Plow::Generator.new(user_name, site_name, site_aliases)
--- spec/plow_spec.rb
+++ spec/plow_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Plow" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "should have correct version" do
+    before(:all) do
+      @expected = "0.0.0"
+    end
+    
+    it "for Plow::VERSION" do
+      Plow::VERSION.should == @expected
+    end
+    
+    it "for VERSION rubygem file" do
+      File.read(File.expand_path(File.join(File.dirname(__FILE__), '..', 'VERSION'))).should == @expected
+    end
   end
 end
