bc2710ed7052c9e02d4becb63056adaaf9d15d69
Ryan Sobol <code@ryansobol.com>
2009-09-20 10:40:57 +0800

    * spec/spec_helper.rb   * Only prepend paths to the $LOAD_PATH if absent

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
 # encoding: UTF-8
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
+unless $LOAD_PATH.include?(File.dirname(__FILE__))
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
+end
+
+unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__) + '/../lib'))
+  $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib'))
+end
+
 
 require 'plow'
 require 'spec'
